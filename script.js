d4d83ae71297e49368dcfe220a48aa33afc174f5
unknown <barisfbturker@gmail.com>
2025-05-19 23:10:05 +0900

    Added the function to update the cursor

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ for (let j = 0; j < 400; j++) {
     gridElement.appendChild(square);
 }
 
+let x = 0;
+let y = 0;
+
+function moveCursor() {
+    const squaresList = document.querySelectorAll(".square");
+    let index = (y * 20 + x);
+
+    for (const squareElement of squaresList) {
+        squareElement.className = "square";
+    }
+
+    squaresList[index].className = "square cursor";
+}
