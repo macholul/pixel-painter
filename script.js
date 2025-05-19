09497a46f574a713641975802861be1398df679e
unknown <barisfbturker@gmail.com>
2025-05-19 23:37:56 +0900

    Added cursor movement and painting logic with keyboard events

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ for (let j = 0; j < 400; j++) {
 
 let x = 0;
 let y = 0;
+const squaresList = document.querySelectorAll(".square");
 
 function moveCursor() {
-    const squaresList = document.querySelectorAll(".square");
     let index = (y * 20 + x);
 
     for (const squareElement of squaresList) {
@@ -19,3 +19,28 @@ function moveCursor() {
 
     squaresList[index].className = "square cursor";
 }
+
+document.addEventListener("keydown", (e) => {
+    let key = e.key;
+
+    if (key === "ArrowUp" && y > 0) {
+        y--;
+        moveCursor();
+
+    } else if (key === "ArrowLeft" && x > 0) {
+        x--;
+        moveCursor();
+
+    } else if (key === "ArrowRight" && x < 19) {
+        x++;
+        moveCursor();
+
+    } else if (key === "ArrowDown" && y < 19) {
+        y++;
+        moveCursor();
+
+    } else if (key === " ") {
+        let index = (y * 20 + x);
+        squaresList[index].className = "square painted";
+    }
+});
